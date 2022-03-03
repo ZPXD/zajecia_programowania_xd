14593b9559be7ecf850e47acb6098c67481cbb66
photovolter <photovolter@gmail.com>
2022-03-03 19:04:22 +0100

    dodanie kodu z zajęć i komentarza do niego

--- 1_piaskownica/012/kod_z_zajec.py
+++ 1_piaskownica/012/kod_z_zajec.py
@@ -1,1 +1,17 @@
-print('TBD xD')
+git clone Xxxxxxxxxxxxxxxx  # na przykład:
+                            #  git clone git@github.com:ZPXD/cwiczenia-git.git
+
+cd Xxxxxxxxxxxxxxxx         # przechodzimy do katalogu ze świeżo sklonowanym repozytorium
+
+# dodaj plik/usun plik      # robimy jakąś zmianę na dysku (np. tworzymy plik)
+
+git add .                   # dodajemy do „poczekalni git-a” („staging area”).
+                            # Kropka oznacza bieżący katalog roboczy - to oznacza,
+                            # że do git'a będą dodane wszystkie zmienione pliki
+                            # z bieżącego katalogu roboczego.
+
+git commit -m "komentarz jakiś" # zatwierdzanie zmian („commit”) -
+                            # wypchnięcie zmienionych plików z „poczekalni” do
+                            # migawki git-a
+git push                    # Wypchnięcie zmian do zdalnego repozytorium
+                            # (np. na Github, lub na własny zdalny serwer)
--- 1_piaskownica/012/notatki.md
+++ 1_piaskownica/012/notatki.md
@@ -2,9 +2,19 @@
 
 YT: https://www.youtube.com/watch?v=P3pi3Co1wDI
 
-15:02 - koniec wprowadzenia
+15:02 - koniec wprowadzenia do zajęć nr 12
 15:34 - określenie narzędzi potrzebych na dzisiejszą lekcję (discord, Github: repozytoria ZPXD i własne)
 17:00 - krótkie wprowadzenie do minimalnych umiejętności obsługi Git-a
+22:45 - Jak forkować (wprowadzać „swoje” odgałęzienie czyjegoś projektu)
+24:30 - Github - wprowadzenie (git clone)
+26:30 - wprowadzanie zmian na dysku lokalnym i synchronizowanie zmian z githubem (git pull -> czyli aktualizacja z serwera Github)
+29:20 - Obsługa git z Visual Studio Code
+30:30 - git clone z terminala VSC
+32:40 - git clone w VSC kliknięciami myszy
+35:24 - komendy terminala VSC do usunięcia repozytorium git (usunięcie folderu: _sudo rm -r cwiecznia-git_)
+
+
+
 
 # 07.03.22 - Start II części zajęć
 - każde zajęcia - 1 mała rzecz + PR
