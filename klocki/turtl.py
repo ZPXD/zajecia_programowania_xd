9ecdf732791de4c6fd910a952331f0566b86d64d
magnuter <paciejsynek@gmail.com>
2022-05-05 17:57:07 +0200

    .

---
+++ klocki/turtl.py
@@ -0,0 +1,12 @@
+from turtle import *
+import tkinter as TK
+
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
