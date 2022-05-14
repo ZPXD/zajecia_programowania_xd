2ddfd0dea0521b5342dc66ca005d98a55eb6a6d0
marcinmxdxd <marcinmxdxd@gmail.com>
2022-05-14 10:33:41 +0000

    Dodaję zadanie z haszera ~marcinmxdxd

--- 3_tbd/031/zadanie_domowe/wasze_zadania/zadanie_haszer.py
+++ 3_tbd/031/zadanie_domowe/wasze_zadania/zadanie_haszer.py
@@ -2,8 +2,6 @@ import sys
 from tower_cybersecurity.scripts.secure_data_hasher import encrypt_file
 
 if __name__ == "__main__":
-    	
-	# Default key.
 	if len(sys.argv) == 2:
 		file_path = sys.argv[1]
 		encrypt_file(file_path)
