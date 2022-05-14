0602f6975a48b287b118a3fe71865cb85859f3d5
marcinmxdxd <marcinmxdxd@gmail.com>
2022-05-14 10:49:42 +0000

    delete

--- 3_tbd/031/zadanie_domowe/wasze_zadania/haszer_zadanie.py
+++
@@ -1,7 +0,0 @@
-import sys
-from tower_cybersecurity.scripts.secure_data_hasher import encrypt_file
-
-if __name__ == "__main__":
-	if len(sys.argv) == 2:
-		file_path = sys.argv[1]
-		encrypt_file(file_path)
